5b50d891e33e205817e8fdfe45dc769abd663419
Elliot Galibert <elliotgalibert@gmail.com>
2023-06-07 16:10:33 +0300

    finished yesterdays started todays

--- Leet_Code/canMakeArithmeticProgression.py
+++ Leet_Code/canMakeArithmeticProgression.py
@@ -1,8 +1,25 @@
 class Solution:
-	def canMakeArithmeticProgression(self, arr: list):
-		pass
-	
+	def canMakeArithmeticProgression(arr: list):
+		neg_index = 0
+		pos_index = arr[1] - arr[0]
+		neg_index = pos_index * -1
+
+		previous = 0
+		difference = (pos_index - neg_index) / (len(arr)- 1)
+		print (pos_index, neg_index, difference)
+
+		for i in range(len(arr) - 1):
+			expected = neg_index + i * difference
+			if expected not in arr:
+				return False
 def main():
 	result = False
-	arr = [3,5,1]
+	# arr = [3,5,1]
+	# arr = [1,2,4]
+	# arr = [1,10,10,10,19]
+	arr = [1,5,6,10]
+	# arr = [0,0,0,0]
 	result = Solution.canMakeArithmeticProgression(arr)
+	print (result)
+
+main()
---
+++ Leet_Code/minFlips.py
@@ -0,0 +1,15 @@
+class Solution:
+	def minFlips(a: int, b: int, c: int) -> int:
+		pass
+
+def main():
+	a = 2
+	b = 6
+	c = 5
+
+	result = 0
+	result = Solution.minFlips(a, b, c)
+	print(result)
+
+
+main()
