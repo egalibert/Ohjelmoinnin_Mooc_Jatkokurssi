5a0d8b4bb3b3099adbdcff1ca20d1227e9e384f4
Elliot Galibert <elliotgalibert@gmail.com>
2023-06-10 18:52:03 +0300

    competition ex1 not finished

---
+++ Leet_Code/Biweekly Contest 106/ex1.py
@@ -0,0 +1,35 @@
+class Solution:
+	def isFascinating(n: int) -> bool:
+		num_str = ""
+		num_str += str(n)
+		num_str += str(n * 2)
+		# print (n, num_str)
+		num_str += str(n * 3)
+		# print (n, num_str)
+		i = 0
+		total = 0
+		numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+		print (f" original string is{num_str} list is {numbers}")
+		while (len(numbers) > i):
+			if  numbers[i] not in num_str:
+				return False
+			if numbers[i] in num_str:
+				print (numbers[i])
+				# numbers.remove(numbers[i])
+				total += 1
+			print (numbers)
+			i += 1
+		print (total)
+		if len(numbers) != 0:
+			return False
+		return True
+    
+def main():
+    # n = 192
+    # n = 100
+    n = 783
+    result = False
+    result = Solution.isFascinating(n)
+    print(result)
+    
+main()
