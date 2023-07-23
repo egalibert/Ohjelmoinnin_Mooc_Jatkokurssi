90405927b27f9efc576fa1ad40f08d8b41fed867
Elliot Galibert <elliotgalibert@gmail.com>
2023-07-23 10:06:00 +0300

    not finished

---
+++ Leet_Code/202.Happy_Number.py
@@ -0,0 +1,15 @@
+class Solution:
+	def isHappy(n: int) -> bool:
+		m = str(n)
+		print (m)
+		i = m.split()
+		print (i)
+	
+def main():
+	n = 19
+	result = False
+	
+	result = Solution.isHappy(n)
+
+main()
+	
