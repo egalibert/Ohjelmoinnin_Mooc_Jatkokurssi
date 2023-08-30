3c6120dc6214c4cd48bd381d7b4f0a2992ded049
Elliot Galibert <elliotgalibert@gmail.com>
2023-08-30 11:12:47 +0300

    Pandas advanced started

---
+++ Codefinity/Pandas_Advanced/Learn_More_About_Indexation.py
@@ -0,0 +1,5 @@
+import pandas as pd
+data = pd.read_csv('https://codefinity-content-media.s3.eu-west-1.amazonaws.com/4bf24830-59ba-4418-969b-aaf8117d522e/people.csv')
+
+print(data.iloc[1]) # CHANGE CODE HERE (to answer the question below)
+print(data.iloc[-1]) # CHANGE CODE HERE (to answer the question below)
